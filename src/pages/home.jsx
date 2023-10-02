3010087c48f26171aed38cd928302735ac64a099
sansanoski <sansanoskiww@gmail.com>
2023-10-02 02:23:18 -0300

    CHANGE PIC

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
         <div className="allContainers">
             <h1>MEU AMOR QUE EU AMOOO MUIITOOO</h1>
             <p>ISSO É SO UM TESTING HEHEHE, TEAMOOOOO</p>
+            <p>SE QUIZER USAR TEM QUE CADASTRAR OU PODE USAR SUA CONTA GOOGLE, BJO TE  AMO CABEÇA DE MAÇA ; )</p>
             <img src="https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29yYSVDMyVBNyVDMyVBM298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"></img>
         </div>
     )
